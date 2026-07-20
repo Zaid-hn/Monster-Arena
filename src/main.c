96baec500285668a0cec3721ae871c628a4c01d9
Zaid Hasan <xenon0ae@gmail.com>
2026-07-20 22:58:40 +0530

    feat: implement player skill system

--- src/main.c
+++ src/main.c
@@ -9,6 +9,8 @@
 #define MAXCLASSES 3
 #define CLASSSTATS 3
 
+#define MAXSKILLS 3
+
 // STRUCTS SECTION //
 
 struct CharacterClass
@@ -27,10 +29,33 @@ struct Character
     int maxhp;
     int currenthp;
 
-    int attack_power;
+    int max_mana;
     int mana;
 
+    int attack_power;
+
     bool isblocking;
+};
+
+struct Skill
+{
+    char skill_name[31];
+
+    int skill_cost;
+    int mana_cost;
+    int damage;
+};
+
+const struct Skill ALL_SKILLS[MAXSKILLS] = {
+    {"Fireball", 0, 10, 14},
+    {"Icicle", 15, 8, 12},
+    {"Poison Ivy", 35, 18, 6}};
+
+struct PlayerSkill
+{
+    const struct Skill *skill;
+
+    bool is_unlocked;
 };
 
 struct Player
@@ -42,6 +67,10 @@ struct Player
     int xp;
     int level_xp;
     int stat_points;
+
+    struct PlayerSkill skills[MAXSKILLS];
+    int skill_points;
+    int skill_count;
 };
 
 struct Enemy
@@ -60,6 +89,17 @@ const struct CharacterClass ENEMYCLASSES[MAXCLASSES] = {
     {"Goblin", 25, 0, 8},
     {"Orc", 120, 10, 43},
     {"Troll", 300, 280, 130}};
+
+// ENUMS SECTION //
+
+enum COMBAT_ACTIONS
+{
+    ATTACK = 1,
+    BLOCK,
+    HEAL,
+    SKILLS,
+    FORFEIT
+};
 
 // FUNCTION PROTOTYPES //
 
@@ -97,6 +137,8 @@ void display_stat_points(struct Player *player);
 
 void initialize_character(const struct CharacterClass characterclass[], struct Character *character);
 
+void initialize_skills(const struct Skill skills[], struct PlayerSkill playerskills[]);
+
 void player_select(struct Player *player);
 
 void enemy_select(struct Enemy *enemy);
@@ -121,6 +163,8 @@ void player_attack(struct Player *player, struct Enemy *enemy);
 
 void enemy_attack(struct Enemy *enemy, struct Player *player);
 
+struct PlayerSkill *choose_skill(struct Player *player);
+
 int player_action();
 
 int enemy_action();
@@ -135,13 +179,27 @@ void combat(struct Player *player, struct Enemy *enemy);
 
 void give_xp(struct Player *player, struct Enemy *enemy);
 
-void level_up(struct Player *player);
-
 int *select_stat(struct Player *player);
 
 void spend_stat_points(struct Player *player, int *stat);
 
 void increase_stats(struct Player *player);
+
+void level_up(struct Player *player);
+
+// SKILLS SYSTEM //
+
+void display_locked_skills(const struct PlayerSkill playerskills[]);
+
+void display_player_skills(const struct Player *player);
+
+struct PlayerSkill *select_skill(struct Player *player);
+
+void buy_skill(struct Player *player, struct PlayerSkill *skill);
+
+void skills_menu(struct Player *player);
+
+void use_skill(struct PlayerSkill *player_skill, struct Player *player, struct Enemy *enemy);
 
 // MAIN SECTION //
 
@@ -216,7 +274,7 @@ int main()
 
 void display_classes(const struct CharacterClass classes[])
 {
-    for (int i = 0; i < 3; i++)
+    for (int i = 0; i < MAXCLASSES; i++)
     {
         printf("\n%d. %s", i + 1, classes[i].class);
     }
@@ -224,7 +282,7 @@ void display_classes(const struct CharacterClass classes[])
 
 void display_stats(struct Character *character)
 {
-    printf("\n\n1. Hp: %d\n\n2. Mana: %d\n\n3. Attack Power: %d\n\n", character->currenthp, character->mana, character->attack_power);
+    printf("\n1. Hp: %d\n\n2. Mana: %d\n\n3. Attack Power: %d\n\n", character->currenthp, character->mana, character->attack_power);
 }
 
 void display_stat_points(struct Player *player)
@@ -251,10 +309,21 @@ void initialize_character(const struct CharacterClass characterclass[], struct C
     character->maxhp = characterclass[classchoice - 1].basehp;
     character->currenthp = characterclass[classchoice - 1].basehp;
 
+    character->max_mana = characterclass[classchoice - 1].basemana;
     character->mana = characterclass[classchoice - 1].basemana;
+
     character->attack_power = characterclass[classchoice - 1].baseattackpower;
 
     character->isblocking = false;
+}
+
+void initialize_skills(const struct Skill skills[], struct PlayerSkill playerskills[])
+{
+    for (int i = 0; i < MAXSKILLS; i++)
+    {
+        playerskills[i].skill = &(skills[i]);
+        playerskills[i].is_unlocked = false;
+    }
 }
 
 void player_select(struct Player *player)
@@ -266,6 +335,12 @@ void player_select(struct Player *player)
     player->xp = 0;
     player->level_xp = 40;
     player->stat_points = 0;
+
+    player->skill_count = 0;
+    player->skill_points = 0;
+
+    initialize_skills(ALL_SKILLS, player->skills);
+    buy_skill(player, &(player->skills[0]));
 }
 
 void enemy_select(struct Enemy *enemy)
@@ -344,10 +419,15 @@ void enemy_attack(struct Enemy *enemy, struct Player *player)
     character_attack(&(enemy->character), &(player->character));
 }
 
+void character_block(struct Character *character)
+{
+    character->isblocking = true;
+}
+
 int player_action()
 {
     int combat_choice;
-    printf("\n1. Attack\n2. Block\n3. Heal\n4. Run away\n\n");
+    printf("\n1. Attack\n2. Block\n3. Heal\n4. Skills\n5. Run away\n\n");
     printf("> ");
     scanf("%d", &combat_choice);
 
@@ -362,6 +442,37 @@ int enemy_action()
     return enemy_combat_choice;
 }
 
+struct PlayerSkill *choose_skill(struct Player *player)
+{
+    int chosen_skill;
+    printf("\n> ");
+    scanf("%d", &chosen_skill);
+
+    if (chosen_skill < 1 || chosen_skill > (player->skill_count))
+    {
+        return NULL;
+    }
+
+    return &(player->skills[chosen_skill - 1]);
+}
+
+void cast_skill(struct Player *player, struct Enemy *enemy)
+{
+    display_player_skills(player);
+    struct PlayerSkill *skill = choose_skill(player);
+    use_skill(skill, player, enemy);
+
+    if (!(skill == NULL))
+    {
+        printf("\nCast %s\n", skill->skill->skill_name);
+        printf("\nEnemy -%d Hp", skill->skill->damage);
+    }
+    else
+    {
+        printf("\nInvalid skill\n");
+    }
+}
+
 bool player_turn(struct Player *player, struct Enemy *enemy)
 {
     player->character.isblocking = false;
@@ -371,20 +482,23 @@ bool player_turn(struct Player *player, struct Enemy *enemy)
 
     switch (choice)
     {
-    case 1:
+    case ATTACK:
         player_attack(player, enemy);
         printf("\nSuccesfully hit\n");
         break;
-    case 2:
-        player->character.isblocking = true;
-        printf("\nYou blocked\n");
+    case BLOCK:
+        character_block(&(player->character));
+        printf("\nYou blocked..\n");
         break;
-    case 3:
+    case HEAL:
         heal(30, player);
         printf("\nSuccessfully healed..");
         printf("\n> Current Hp: %d\n", player->character.currenthp);
         break;
-    case 4:
+    case SKILLS:
+        cast_skill(player, enemy);
+        break;
+    case FORFEIT:
         continue_combat = false;
         printf("\nYou ran away from the fight...\n");
         break;
@@ -409,8 +523,8 @@ void enemy_turn(struct Player *player, struct Enemy *enemy)
         printf("\n> Current Hp: %d\n", player->character.currenthp);
         break;
     case 2:
-        enemy->character.isblocking = true;
-        printf("\nEnemy blocked..\n");
+        character_block(&(enemy->character));
+        printf("\nEnemy blocked!!\n");
         break;
     }
 }
@@ -445,6 +559,8 @@ void combat(struct Player *player, struct Enemy *enemy)
     }
 }
 
+// ------------------------------------------------------- //
+
 void level_up(struct Player *player)
 {
     if (player->xp == player->level_xp)
@@ -455,7 +571,10 @@ void level_up(struct Player *player)
         player->xp -= player->level_xp;
         player->stat_points += 10;
         player->level_xp *= 2;
+
+        player->skill_points += 15;
         increase_stats(player);
+        skills_menu(player);
     }
 }
 
@@ -471,7 +590,7 @@ int *select_stat(struct Player *player)
         break;
 
     case 2:
-        return &(player->character.mana);
+        return &(player->character.max_mana);
         break;
 
     case 3:
@@ -494,7 +613,7 @@ void spend_stat_points(struct Player *player, int *stat)
         if (!(stat == NULL))
         {
             *stat += invest_points;
-            printf("\nStat increased by %d", invest_points);
+            printf("\nStat increased by %d\n", invest_points);
 
             player->stat_points -= invest_points;
         }
@@ -520,4 +639,101 @@ void increase_stats(struct Player *player)
 
     printf("\nSpend stat points: ");
     spend_stat_points(player, stat);
+}
+
+// ------------------------------------------------------- //
+
+void display_locked_skills(const struct PlayerSkill playerskills[])
+{
+    int unlocked_skills = 0;
+
+    for (int i = 0; i < MAXSKILLS; i++)
+    {
+        if (!(playerskills[i].is_unlocked))
+        {
+            printf("\n%d. %s (%d SP)", (i - unlocked_skills) + 1, playerskills[i].skill->skill_name, playerskills[i].skill->skill_cost);
+        }
+        else
+        {
+            unlocked_skills++;
+        }
+    }
+}
+
+void display_player_skills(const struct Player *player)
+{
+    for (int i = 0; i < player->skill_count; i++)
+    {
+        printf("\n%d. %s", i + 1, player->skills[i].skill->skill_name);
+    }
+}
+
+struct PlayerSkill *select_skill(struct Player *player)
+{
+    int chosen_skill;
+    printf("\nLearn skill: ");
+    scanf("%d", &chosen_skill);
+
+    if (chosen_skill < 1 || chosen_skill > (MAXSKILLS - player->skill_count))
+    {
+        return NULL;
+    }
+
+    return &(player->skills[(chosen_skill - 1) + player->skill_count]);
+}
+
+void buy_skill(struct Player *player, struct PlayerSkill *skill)
+{
+    if (!(skill == NULL))
+    {
+        if (player->skill_points >= skill->skill->skill_cost)
+        {
+            skill->is_unlocked = true;
+            player->skill_count++;
+
+            player->skill_points -= skill->skill->skill_cost;
+        }
+        else
+        {
+            printf("Insufficient SP!!");
+        }
+    }
+    else
+    {
+        printf("Invalid skill");
+    }
+}
+
+void skills_menu(struct Player *player)
+{
+    display_locked_skills(player->skills);
+
+    printf("\nCurrent SP: %d\n", player->skill_points);
+    struct PlayerSkill *skill = select_skill(player);
+
+    buy_skill(player, skill);
+
+    if (!(skill == NULL))
+    {
+        printf("\nLearnt %s\n", skill->skill->skill_name);
+    }
+}
+
+void use_skill(struct PlayerSkill *player_skill, struct Player *player, struct Enemy *enemy)
+{
+    if (!(player_skill == NULL))
+    {
+        if (player_skill->is_unlocked)
+        {
+            if (player->character.mana >= player_skill->skill->mana_cost)
+            {
+                player->character.mana -= player_skill->skill->mana_cost;
+                take_damage(player_skill->skill->damage, &(enemy->character));
+            }
+            else
+            {
+                printf("\nInsufficient mana!!\n");
+            }
+        }
+    }
 }
